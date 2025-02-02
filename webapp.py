49c11297341b6baf0ac6a725ec263f065a2aca41
Lefaun <118660975+Lefaun@users.noreply.github.com>
2025-02-02 13:20:35 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -97,12 +97,12 @@ if usuario == "admin" and senha == "1234":
             {"nome": "Cesta Orgânica", "preco": 12.99, "img": "Horta.png"},
             {"nome": "Sabonete Natural", "preco": 7.50, "img": "soap.png"},
             {"nome": "Bolsa Ecológica", "preco": 15.00, "img": "https://via.placeholder.com/150"},
-            {"nome": "Kit Bambu", "preco": 9.99, "img": "https://via.placeholder.com/150"},
+            {"nome": "Kit Bambu", "preco": 9.99, "img": "KitBambu.png"},
             {"nome": "Mel Orgânico", "preco": 18.50, "img": "https://via.placeholder.com/150"},
             {"nome": "Horta Caseira", "preco": 25.00, "img": "https://via.placeholder.com/150"},
-            {"nome": "Cosméticos Naturais", "preco": 19.99, "img": "https://via.placeholder.com/150"},
+            {"nome": "Cosméticos Naturais", "preco": 19.99, "img": "BolsaCosmetico.png"},
             {"nome": "Chá Artesanal", "preco": 10.99, "img": "https://via.placeholder.com/150"},
-            {"nome": "Velas Ecológicas", "preco": 14.50, "img": "https://via.placeholder.com/150"},
+            {"nome": "Velas Ecológicas", "preco": 14.50, "img": "Velas.png"},
         ]
 
         # Inicializar carrinho
